53692aacb94f4cfbeec0a27ed3ecdd9c09825521
Sourabh Verma <sourabh.coder@gmail.com>
2015-11-12 20:28:18 +0530

    Use source.unsplash

--- _includes/js/header_image.js
+++ _includes/js/header_image.js
@@ -4,15 +4,15 @@
     console.log($header);
     var width = $header.width();
     var height = $header.height();
-    var url = 'https://unsplash.it/' + width + '/' + height + '/?random';
-    $header.css('background-image', "url('" + url + "')");
+    var url = 'https://source.unsplash.com/random/' + width + 'x' + height;
+    $header.css('background-image', 'url(' + url + ')');
 
     var $footer = $('footer');
     console.log($footer);
     var width = $footer.width();
     var height = $footer.height();
-    var url = 'https://unsplash.it/' + width + '/' + height + '/?random';
-    $footer.css('background-image', "url('" + url + "')");
+    var url = 'https://source.unsplash.com/random/' + width + 'x' + height;
+    $footer.css('background-image', 'url(' + url + ')');
 
 
 }) (jQuery);
