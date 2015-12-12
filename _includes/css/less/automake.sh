3360d62c4dce10f7c77257da86901a2f594d7de5
Sourabh Verma <sourabh.coder@gmail.com>
2015-12-12 18:55:48 +0530

    Minor fixes

--- _includes/css/less/automake.sh
+++ _includes/css/less/automake.sh
@@ -2,10 +2,9 @@
 
 function _make () {
     read path
-    filename=$(basename $path | cut -f1 -d'.');
+    filename=$(basename $path | cut -f1 -d'.')
     make "$filename.css"
 }
 
-make
 fswatch -0 *.less | xargs -0 -n1 -I{} make
 # fswatch *.less | _make
--- _includes/js/header_image.js
+++ _includes/js/header_image.js
@@ -1,18 +1,19 @@
 (function ($) {
 
+    // set header image
     var $header = $('header');
     console.log($header);
     var width = $header.width();
     var height = $header.height();
-    var url = 'https://source.unsplash.com/random/' + width + 'x' + height;
+    var url = 'https://source.unsplash.com/random/' + width + 'x'; // + height; // removing height speeds up image fetch
     $header.css('background-image', 'url(' + url + ')');
 
+    // set footer image
     var $footer = $('footer');
     console.log($footer);
     var width = $footer.width();
     var height = $footer.height();
-    var url = 'https://source.unsplash.com/random/' + width + 'x' + height;
-    $footer.css('background-image', 'url(' + url + ')');
-
+    var url = 'https://source.unsplash.com/random/' + width + 'x'; // + height; // removing height speeds up image fetch
+    // $footer.css('background-image', 'url(' + url + ')');
 
 }) (jQuery);
